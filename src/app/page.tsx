97e9ca0a573679b2aef78f288a8c0a6a3a9c44ad
H-Janath <janathhma@gmail.com>
2025-01-05 18:01:52 +0530

    Add SVG icons for various functionalities and update homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-      <div>
-        <h1  className="text-white text-3xl underline">Home</h1>
-        <Button>Click me</Button>
-      </div>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              @2025 CarePluse
+            </p>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
 }
