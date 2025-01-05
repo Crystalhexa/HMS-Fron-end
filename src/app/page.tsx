954db4e102496dbdfda1ffec1f754a89e248263a
H-Janath <janathhma@gmail.com>
2025-01-05 23:14:42 +0530

    Add PatientForm component with form validation and loading state; include custom SubmitButton and Radix UI select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { PatientForm } from "@/components/form/PatientForm";
 import Image from "next/image";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
 
+          <PatientForm/>
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
               @2025 CarePluse
--- src/components/form/PatientForm.tsx
+++ src/components/form/PatientForm.tsx
@@ -1,12 +1,79 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-const PatientForm = () => {
-    
+import { Form } from "@/components/ui/form";
+import { UserFormValidation } from "@/lib/validation";
+
+import "react-phone-number-input/style.css";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+
+
+export const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      username: "",
+      password: ""
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+
+    try {
+      const user = {
+        username: values.username,
+        password: values.password
+      };
+
+
+
+      // if (newUser) {
+      //   router.push(`/patients/${newUser.$id}/register`);
+      // }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
-    <div>PatientForm</div>
-  )
-}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Get started with Signin.</p>
+        </section>
 
-export default PatientForm
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="username"
+          label="User name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="password"
+          label="Password"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
+};
